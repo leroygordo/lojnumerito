fa43f24945244494674579454e46317a48b6923d
Hancel Gonzalez <hancel.gonzalez@gmail.com>
2012-10-21 14:21:57 -0430

    clase y metodos de node

--- node.cpp
+++ node.cpp
@@ -1,45 +1,59 @@
-#include "node.hpp"
+//#include "node.hpp"
 
-node::state::state(char *board)
-  :board(board)
-{
-}
+#include <list>
+#include <utility>
+#include <iostream>
+#include <string.h>
 
-node::state::~state(){
-}
+using namespace std;
 
-bool node::state::is_goal(){
-}
-
-list<tuple<node::state,char> > node::state::succ(){
-}
-
-node::node(node::state &s, node *p, char a, int c)
-  :s(s),parent(p),action(a),cost(c){
-}
-
-node::~node(){
-}
-
-node make_root(node::state s){
-  node n = node(s,0,0,0);
-  return n;
-}
+class node {
+  public:
+  struct state {
+    int *board;
+	
+	state(int *board):board(board){}
+    ~state(){}
+	bool is_goal(){
+	  for(int i = 0; i < 15 ; i++)
+	    if(board[i] != i) {
+		  return false;
+		}
+	}
+	list<pair<node::state,char> > succ(){}
+  };
   
-node make_node(node *n, char a, node::state s,int c){
-  node n_ = node(s,n,a,c);
-  return n_;
-}
-  
-list<char> extract_solution(node * n){
-  list<char> path;
-  node * n_ = n;
-  while (!n_->parent) {
-    path.push_front(n_->action);
-	n_ = n_->parent;
+  public:
+  state s;
+  node *parent;
+  char action;
+  int cost;
+
+  node():s(0),parent(0),action(0),cost(0){}
+  node(node::state &s, node *p, char a, int c):s(s),parent(p),action(a),cost(c){}
+  ~node(){}
+  node make_root(node::state s){
+    node n = node(s,0,0,0);
+    return n;
   }
-  return path;
-}
+  node make_node(node *n, char a, node::state s,int c){
+    node n_ = node(s,n,a,c);
+    return n_;
+  }
+  list<char> extract_solution(){
+    list<char> path;
+    node *n_ = parent;
+    while (!n_->parent) {
+      path.push_front(n_->action);
+	  n_ = n_->parent;
+    }
+    return path;
+  }  
+};
 
+bool operator<(const node &n1, const node &n2) {
+ return (n1.cost < n2.cost);
+}
+  
 int main(int argc,char* argv[]) {
 }
