e0e627130e609768cae455a29ef13f5bd4aa8ab9
Hancel Gonzalez <hancel.gonzalez@gmail.com>
2012-10-23 14:19:00 -0430

    node y algorithms

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,8 +1,51 @@
-#include "node.hpp"
+#include "node.cpp"
 #include <iostream>
+#include <list>
+#include <queue>
+#include <utility>
+#include <vector>
+#include <set>
+#include <stdlib.h>
+
+using namespace std;
+
+bool isInSet(node::state s, set<node::state> states) {
+}
+
+/*list<char> a_star(char board[]) {
+  list<char> solution;
+  priority_queue<node> queue_;
+  node root;
+  root.make_root(node::state(board));
+  queue_.push(root);
+  set<node::state> closed;
+  
+  while (!queue_.empty()) {
+    node n; 
+	n = queue_.top();
+	queue_.pop();
+	if (!isInSet(n.s,closed)) {
+	  closed.insert(n.s);
+	  if(n.s.is_goal()) {
+	    solution = n.extract_solution();
+	  }
+	  list<pair<node::state,char> > succ_ = n.s.succ();
+	  for(list<pair<node::state,char> >::iterator iter=succ_.begin(),limit=succ_.end() ; iter != limit ; iter++) {
+	    node n_;
+		n_.make_node(n,(*iter).second,(*iter).first,0);
+	  }
+	}
+  }
+  return solution;
+}*/
 
 int main(int argc,char* argv[]) {
-
-  cout << "ANYWAY" << endl;
+  char b[16] = {1,12,2,3,4,5,6,7,8,9,10,11,0,13,14,15};
+  node::state s = node::state(b);
+  
+  node* r; 
+  r->make_root(s);
+  
+  cout << r->cost << endl;
 
 }
--- node.cpp
+++ node.cpp
@@ -1,26 +1,67 @@
-//#include "node.hpp"
-
 #include <list>
 #include <utility>
 #include <iostream>
 #include <string.h>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory>
 
 using namespace std;
 
 class node {
   public:
   struct state {
-    int *board;
+    char board[16];
+	int blank;
 	
-	state(int *board):board(board){}
+	state(char b[]){
+	  for (int i = 0 ; i < 16 ; i++) {
+	    board[i] = b[i];
+		if ((int) board[i] == 0)
+		  blank = i;
+	  }
+	}
+	
     ~state(){}
+	
 	bool is_goal(){
-	  for(int i = 0; i < 15 ; i++)
-	    if(board[i] != i) {
+	  for(int i = 0; i <= 15 ; i++) {
+	    if((int) board[i] != i) {
 		  return false;
 		}
+	  }
+	  return true;
 	}
-	list<pair<node::state,char> > succ(){}
+	
+	list<pair<node::state,char> > succ(){
+	  list<pair<node::state,char> > succ_;
+	  if((blank - 4) >= 0) {
+	    state s = state(board);
+		swap(s.board[blank],s.board[blank-4]);
+		s.blank = blank - 4;
+	    succ_.push_back(make_pair(s,'U'));
+	  }
+	  if((blank + 4) <= 15) {
+	  	state s = state(board);
+		swap(s.board[blank],s.board[blank+4]);		
+		s.blank = blank + 4;
+	    succ_.push_back(make_pair(s,'D'));
+	  }
+	  if((blank + 1) % 4 != 0) {
+	  	state s = state(board);
+		swap(s.board[blank],s.board[blank+1]);	  
+		s.blank = blank + 1;
+	    succ_.push_back(make_pair(s,'R'));
+	  }
+	  if(!(blank % 4 == 0)) {
+	  	state s = state(board);
+		swap(s.board[blank],s.board[blank-1]);
+		s.blank = blank - 1;
+	    succ_.push_back(make_pair(s,'L'));
+	  }
+	  return succ_;
+	}
   };
   
   public:
@@ -29,17 +70,26 @@ class node {
   char action;
   int cost;
 
-  node():s(0),parent(0),action(0),cost(0){}
-  node(node::state &s, node *p, char a, int c):s(s),parent(p),action(a),cost(c){}
+  node()
+    :s(0),parent(0),action(0),cost(0)
+  {}
+  
+  node(node::state &s, node *p, char a, int c)
+    :s(s),parent(p),action(a),cost(c)
+  {}
+  
   ~node(){}
+  
   node make_root(node::state s){
     node n = node(s,0,0,0);
     return n;
   }
+  
   node make_node(node *n, char a, node::state s,int c){
     node n_ = node(s,n,a,c);
     return n_;
   }
+  
   list<char> extract_solution(){
     list<char> path;
     node *n_ = parent;
@@ -53,7 +103,4 @@ class node {
 
 bool operator<(const node &n1, const node &n2) {
  return (n1.cost < n2.cost);
-}
-  
-int main(int argc,char* argv[]) {
 }
